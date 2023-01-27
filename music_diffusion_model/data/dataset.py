448e8c75153484fbee5e8f33cd564a108cc9bd42
samuel <berriens@gmail.com>
2023-01-27 13:21:29 +0100

    mnist dataset

---
+++ music_diffusion_model/data/dataset.py
@@ -0,0 +1,27 @@
+import gzip
+import pickle as pkl
+from os.path import abspath, dirname, join
+
+import torch as th
+from torch.utils.data import Dataset
+
+_RESOURCE_FOLDER = abspath(join(dirname(__file__), "..", "resources"))
+
+
+class MNISTDataset(Dataset):
+    def __init__(self) -> None:
+        super().__init__()
+
+        (mnist_train, _), _ = pkl.load(
+            gzip.open(join(_RESOURCE_FOLDER, "mnist.pkl.gz"), "rb"),
+            encoding="bytes",
+        )
+
+        self.__tensor = th.from_numpy(mnist_train)
+
+    def __getitem__(self, index: int) -> th.Tensor:
+        # None -> one channel
+        return self.__tensor[index, None]
+
+    def __len__(self) -> int:
+        return self.__tensor.size(0)
--- setup.py
+++ setup.py
@@ -4,7 +4,16 @@ setup(
     name="music_diffusion_model",
     author="Samuel Berrien",
     version="1.0",
-    packages=["music_diffusion_model", "music_diffusion_model.networks"],
+    packages=[
+        "music_diffusion_model",
+        "music_diffusion_model.networks",
+        "music_diffusion_model.data",
+    ],
+    package_data={
+        "music_diffusion_model": [
+            "resources/*",
+        ]
+    },
     url="https://github.com/Ipsedo/MusicDiffusionModel",
     license="GPL-3.0 License",
 )
