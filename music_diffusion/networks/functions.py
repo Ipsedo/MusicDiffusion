2fb35e7c80677816b0407c97601c548983034325
samuel <berriens@gmail.com>
2023-05-20 01:06:39 +0200

    refactoring + download youtube channel notebook

--- music_diffusion/networks/functions.py
+++ music_diffusion/networks/functions.py
@@ -5,21 +5,6 @@ import torch as th
 from torch.distributions import Normal
 
 
-def smart_clip(x: th.Tensor, min_value: float, max_value: float) -> th.Tensor:
-    value_range = max_value - min_value
-
-    x = (x - min_value) / value_range % 1.0
-    x[x < 0.0] += 1.0
-
-    x = x * value_range + min_value
-
-    return x
-
-
-def bound_clip(x: th.Tensor, min_value: float, max_value: float) -> th.Tensor:
-    return th.clip(x, min_value, max_value)
-
-
 def normal_log_prob(
     x: th.Tensor, mu: th.Tensor, sigma: th.Tensor
 ) -> th.Tensor:
