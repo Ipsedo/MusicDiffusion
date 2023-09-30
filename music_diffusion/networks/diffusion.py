try fixing NaN -> set limit for beta tiddle to 1e-10, add epsilon of 1e-10 for kl & nll division

music_diffusion/networks/diffusion.py
@@ -41,7 +41,6 @@ class Diffuser(ABC, nn.Module):
         alphas_cum_prod_prev = f_values[:-1] / f_values[0]
 
         betas = 1 - alphas_cum_prod / alphas_cum_prod_prev
-        # 1e-4 and 0.999
         betas = th.clamp(betas, 0.0, 0.999)
 
         alphas = 1 - betas
@@ -57,7 +56,8 @@ class Diffuser(ABC, nn.Module):
         betas_tiddle = (
             betas * (1.0 - alphas_cum_prod_prev) / (1.0 - alphas_cum_prod)
         )
-        self._betas_tiddle_limit = 1e-20
+
+        self._betas_tiddle_limit = 1e-10
         betas_tiddle = th.clamp_min(betas_tiddle, self._betas_tiddle_limit)
 
         # attributes definition

music_diffusion/networks/functions.py
@@ -56,8 +56,8 @@ def normal_kl_div(
     var_1: th.Tensor,
     mu_2: th.Tensor,
     var_2: th.Tensor,
-    epsilon: float = 1e-20,
-    clip_max: float = 16.0,
+    epsilon: float = 1e-10,
+    clip_max: float = 64.0,
 ) -> th.Tensor:
     return (
         (
@@ -110,8 +110,8 @@ def negative_log_likelihood(
     x: th.Tensor,
     mu: th.Tensor,
     var: th.Tensor,
-    epsilon: float = 1e-20,
-    clip_max: float = 16.0,
+    epsilon: float = 1e-10,
+    clip_max: float = 64.0,
 ) -> th.Tensor:
     return (
         (
@@ -121,30 +121,3 @@ def negative_log_likelihood(
         .clamp(0, clip_max)
         .mean(dim=[2, 3, 4])
     )
-
-
-def normal_js_div(
-    mu_1: th.Tensor,
-    var_1: th.Tensor,
-    mu_2: th.Tensor,
-    var_2: th.Tensor,
-    epsilon: float = 1e-20,
-) -> th.Tensor:
-
-    kl1 = (
-        th.log(var_2 + epsilon) / 2.0
-        - th.log(var_1 + epsilon) / 2.0
-        + (var_1 + th.pow(mu_1 - mu_2, 2.0) + epsilon) / (2 * var_2 + epsilon)
-        - 0.5
-    )
-    kl2 = (
-        th.log(var_1 + epsilon) / 2.0
-        - th.log(var_2 + epsilon) / 2.0
-        + (var_2 + th.pow(mu_2 - mu_1, 2.0) + epsilon) / (2 * var_1 + epsilon)
-        - 0.5
-    )
-
-    jsd = 0.5 * (kl1 + kl2)
-    jsd = th.mean(jsd, dim=[2, 3, 4])
-
-    return jsd